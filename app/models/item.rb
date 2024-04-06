50ca1673916c2f6fb1702ed3c85793142f27e2f7
yuuri312 <yz5@outlook.jp>
2024-04-06 23:10:17 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :due_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :paid_shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
