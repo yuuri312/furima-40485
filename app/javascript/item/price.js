d06fae43f453a2381614cd23ae3d24cc9675ad18
yuuri312 <yz5@outlook.jp>
2024-04-07 23:21:35 +0900

    importMapの設定を変更

---
+++ app/javascript/item/price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById('item-price');
+  const addTax = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTax.innerHTML = (Math.floor(inputValue/10));
+    profit.innerHTML = inputValue - (Math.floor(inputValue/10));
+  })
+
+})
