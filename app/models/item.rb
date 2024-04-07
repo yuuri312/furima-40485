031af67bad47a4349efed38c5e5321ad75d7129e
yuuri312 <yz5@outlook.jp>
2024-04-07 22:14:54 +0900

    入力された価格によって、販売手数料や販売利益の表示が変わること。

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,8 @@ class Item < ApplicationRecord
   validates :due_date_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :paid_shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-
-
+  belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -18,7 +18,4 @@ class Item < ApplicationRecord
   belongs_to :paid_shipping_cost
   belongs_to :prefecture
   belongs_to :due_date
-  has_one_attached :image
-
-
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"ジャケット"}
+    description {"古着です"}
+    category_id {2}
+    condition_id {3}
+    paid_shipping_cost_id {2}
+    prefecture_id {3}
+    due_date_id {3}
+    price {987}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
